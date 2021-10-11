9b66feb163730bb602b9ac26cc0aa6d3c957de41
Michelly Ferreira <dev.michelly.f@gmail.com>
2021-10-11 02:09:58 -0300

    Conhecendo Docker

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
-import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+import { router } from "./routes";
 
 const app = express();
 
