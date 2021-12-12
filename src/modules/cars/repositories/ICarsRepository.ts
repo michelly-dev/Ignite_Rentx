a9d1fcd93855872ab96bb52d4b46983b971f4a35
Michelly Ferreira <dev.michelly.f@gmail.com>
2021-12-12 20:13:35 -0300

    Uso do cadastro de especificação para carro

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,7 +4,10 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>
   findByLicensePlate(license_plate: string): Promise<Car>;
-  findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]>;
+  findAvailable(brand?: string,
+    category_id?: string,
+    name?: string): Promise<Car[]>;
+  findById(id: string): Promise<Car>;
 }
 
 export { ICarsRepository };
--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -45,6 +45,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
     return all;
   }
+
+  async findById(id: string): Promise<Car> {
+    return this.cars.find((car) => car.id === id);
+  }
 }
 
 export { CarsRepositoryInMemory };
